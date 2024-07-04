b715474bad2317421a91ab7539ce366289060149
Ege Esin <egesin03@gmail.com>
2024-07-04 16:40:11 +0200

    lol

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define x values for plotting (avoiding zero)
+#Define x values for plotting (avoiding zero)
 x_values = np.linspace(0.1, 10, 400)
 ln_x_values = np.log(x_values)
 ln_P_greater_x = -x_values ** 2  # Since P[X > x] = exp(-x^2) for beta=2, lambda=1
